9742ed8a0cceac8d373d71e92b13157995e733ca
Duapar13 <bastien.rapaud@epitech.eu>
2026-07-08 15:35:46 +0200

    feat: tâche d'expiration automatique
    Vérifie périodiquement les annonces actives arrivées à expiration et
    prévient le vendeur (s'il est connecté) qu'il peut récupérer son
    objet via /ah reclaim.

---
+++ src/main/java/com/dauction/tasks/ExpirationTask.java
@@ -0,0 +1,32 @@
+package com.dauction.tasks;
+
+import com.dauction.manager.AuctionManager;
+import com.dauction.util.Display;
+import com.dauction.util.Msg;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * Vérifie périodiquement les annonces actives arrivées à expiration et prévient
+ * le vendeur (s'il est connecté) qu'il peut récupérer son objet via /ah reclaim.
+ */
+public class ExpirationTask extends BukkitRunnable {
+
+    private final AuctionManager auctionManager;
+
+    public ExpirationTask(AuctionManager auctionManager) {
+        this.auctionManager = auctionManager;
+    }
+
+    @Override
+    public void run() {
+        auctionManager.expireOldListings(listing -> {
+            Player seller = Bukkit.getPlayer(listing.getSellerId());
+            if (seller != null) {
+                Msg.send(seller, "Ta mise en vente #" + listing.getId() + " (" + Display.itemName(listing.getItem())
+                        + ") a expiré sans trouver preneur. Récupère l'objet avec /ah reclaim.");
+            }
+        });
+    }
+}
